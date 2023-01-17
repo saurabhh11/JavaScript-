22a2dba97c85130d1f55836a30bda7bb6525c67c
saurabhh11 <118429795+saurabhh11@users.noreply.github.com>
2023-01-17 10:28:47 +0530

    Update assignment1.js

--- assignment1.js
+++ assignment1.js
@@ -1,3 +1,5 @@
+1. Write a program to find whether a given year is a leap year or not.
+
 function isLeapYear(year) {
   if (year % 4 !== 0) {
     return false;
@@ -9,3 +11,30 @@ function isLeapYear(year) {
 console.log(isLeapYear(2000)); 
 console.log(isLeapYear(1900));
 console.log(isLeapYear(2024));
+
+2.Write a JavaScript program to convert temperatures to and from Celsius,
+Fahrenheit.
+
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+  }
+  
+  function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+  
+  console.log(celsiusToFahrenheit(60) + "°F");
+  console.log(fahrenheitToCelsius(45) + "°C"); 
+  
+3.Write a program to find the factorial of a number.
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+    return n * factorial(n-1);
+  }
+  console.log(factorial(7)); 
+  console.log(factorial(8)); 
+  
