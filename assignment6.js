aceb0d3d8924c66752b80ed406665a90a581cfd0
saurabhh11 <118429795+saurabhh11@users.noreply.github.com>
2023-01-20 16:47:57 +0530

    Update assignment6.js

--- assignment6.js
+++ assignment6.js
@@ -1,3 +1,12 @@
+1) Perform the following operations to provide the implementation for a
+Rectangle class. The operations are:
+1. Add an area() method to the Rectangle class.
+2. Create a Square class that satisfies the following conditions:
+○ It is a subclass of Rectangle.
+○ It contains a constructor and no other methods.
+○ It can use the Rectangle class' area method to print the area
+of a Square object.
+
 class Rectangle {
     constructor(width, height) {
         this.width = width;
@@ -19,6 +28,8 @@ console.log(square.area());
 let rectangle = new Rectangle(5,10);
 console.log(rectangle.area()); 
 
+2) Write a javascript function find_largest to return the nth largest number in an array-
+
 
 function find_largest(arr, n) {
     // sort the array in descending order
@@ -29,6 +40,9 @@ function find_largest(arr, n) {
 let arr = [3,45,8,6,21,54,11,12];
 console.log(find_largest(arr, 5)); 
 
+3) Write a JavaScript program which accept a number as input in the
+function parameter and insert dashes (-) between each two even
+numbers.
 
 function computeDash(num) {
     // convert the number to a string
