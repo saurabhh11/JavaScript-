0d56711a00055f9c5754d445ff6ce9df7da0c1f2
saurabhh11 <118429795+saurabhh11@users.noreply.github.com>
2023-01-20 17:01:21 +0530

    Update assignment13.js

--- assignment13.js
+++ assignment13.js
@@ -1,3 +1,6 @@
+1. Write a JavaScript program to get an array from the user and return the:
+a) Sum of all elements in the array using reduce()
+b) Average of all elements in the array using reduce()
 
 // Using reduce() to get the sum of all elements in the array
 const array = [ 1, 2, 3, 4, 5 ];
@@ -7,6 +10,17 @@ console.log(sum); // Output: 15
 // Using reduce() to get the average of all elements in the array
 const average = array.reduce((acc, curr) => acc + curr, 0) / array.length;
 console.log(average); 
+
+2. Write a JavaScript program to
+a) Calculate grades on basis of marks
+>90 = A
+>80 = B
+>70 = C
+>60 = D
+>50 = E
+else = F
+b) Map the grades of each student
+c) Group students according to the grades they have received and display.
 
 let students = [
     { name: "John", marks: 92 },
